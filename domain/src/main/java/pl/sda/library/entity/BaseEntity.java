033e65ab32dfdbbf9d62e507d7a35a6f847fa419
maciejskro <maciejskro@gmail.com>
2018-06-03 22:16:05 +0200

    create some services

--- domain/src/main/java/pl/sda/library/entity/BaseEntity.java
+++ domain/src/main/java/pl/sda/library/entity/BaseEntity.java
@@ -1,12 +1,10 @@
 package pl.sda.library.entity;
 
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 
+@MappedSuperclass
 public abstract class BaseEntity  implements Serializable {
 
     @Id
--- persistence/src/main/resources/import_2.sql
+++ persistence/src/main/resources/import_2.sql
@@ -1,4 +1,2 @@
 INSERT INTO borrower( firstName , lastName, username, password )
              VALUES ( 'maciek' , 'skrobek' , 'maciejskro', 'start123');
-
-select * from borrower;
